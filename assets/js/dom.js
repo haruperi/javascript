eb841f64f5b08f00a1ccef2316e1d8558b7c330a
haruperi <rufaroh59@gmail.com>
2021-11-19 09:46:30 +0000

    Shopping list before my input

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -28,3 +28,40 @@
 // children
 
 // ##It is important to CACHE selectors in variables
+
+const h1 = document.querySelector("h1")
+const button = document.querySelector("#enter");
+const input = document.querySelector("#userinput");
+const ul = document.querySelector("ul");
+const cancel = document.querySelector("li")
+
+h1.className = "cooltitle"
+//cancel.className = "done"
+
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	const li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
