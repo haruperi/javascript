dfb4114a55dce65c2d481d78b0bfe320edb95e9a
haruperi <rufaroh59@gmail.com>
2021-11-19 06:48:46 +0000

    Cleaned it up

---
+++ assets/js/exercises.js
@@ -0,0 +1,74 @@
+// Program a Calculator
+const calculator = (num1,sym,num2) => {
+    switch (sym) {
+        case "+":
+            return num1+num2
+            break;
+        case "-":
+            return num1-num2
+            break;
+        case "*":
+            return num1*num2
+            break;
+        case "/":
+            return num1/num2
+            break;
+    
+        default:
+            break;
+    }
+}
+const ans = calculator(10,"/",2)
+
+
+/************************************************************************************************************************************************************************** */
+// using this array,
+let array = ["Banana", "Apples", "Oranges", "Blueberries"]
+// 1. Remove the Banana from the array.
+array.shift()
+// 2. Sort the array in order.
+array.sort()
+// 3. Put "Kiwi" at the end of the array.
+array.push("Kiwi")
+// 4. Remove "Apples" from the array.
+array.shift()
+// 5. Sort the array in reverse order. (Not alphabetical, but reverse
+// the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
+array.reverse()
+//you should have at the end:
+//["Kiwi", "Oranges", "Blueberries"]
+
+// using this array,
+const array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges".
+
+let access = array2[1][1][0]
+
+/************************************************************************************************************************************************************************** */
+// Create an object and an array which we will use in our facebook exercise. 
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+const facebook = {
+    username : "haruperi",
+    password : "123"
+};
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+const database = [facebook]
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+let newsfeed = [
+    {
+		username: "Bobby",
+		timeline: "So tired from all that learning!"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript is sooooo cool!"
+	},
+	{
+		username: "Mitch",
+		timeline: "Javascript is preeetyy cool!"
+	}
+];
+
+access = database[0].username
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -37,6 +37,10 @@ let userObject  = {
 // 3.0 JAVASCRIPT VARIABLES
 const constNumber = 1988
 let varName = "Name"
+let sum = 0
+
+
+
 
 
 // 4.0 JAVASCRIPT FLOW CONTROL - CONDITIONALS
@@ -97,23 +101,81 @@ if (varIf) {
 
 // 6.0 JAVASCRIPT FUNCTIONS
 
-  const squareRoot =  (num) => {
-    return num*num
-}
+  const squareRoot =  (num) =>  num*num
+  const multiply = (a) => (b) => a*b;
 
 
 
-// 6.0 JAVASCRIPT DATA STRUCTURES
+// 7.0 JAVASCRIPT DATA STRUCTURES
 // Arrays
  let arrVar = [1,2,3,4,5]
+ //foreach
+const forEachArr = arrVar.forEach(i => sum+=squareRoot(i) )
+//map
+const mapArr = arrVar.map(i => squareRoot(i) )
+//filter
+const filterArr = arrVar.filter(i => i>3)
+//reduce
+const reduceArr = arrVar.reduce((sum,i) => {
+    return sum+i
+},0);
+
+//debug
+
+
+const flattened = [[0,1],[2,3],[4,5]].reduce((accumulator,innerArray) => {
+    return accumulator.concat(innerArray)},[]);
+
+
 
 //  Object
-let userObject  = {
+let userObject1  = {
     userName : "Rufaro",
     age : 33
 };
 
-console.log(squareRoot(4))
+//referance type
+let object1 = {value : 10 };
+let object2 = object1
+let object3 = {value: 10 };
+// object1 == object3 will return false because these are not values but referances to objects and they will always be differant with each object
+
+// context vs scope
+// instantiation
+class Player {
+    constructor(name, type){
+        this.name = name
+        this.type = type
+    }
+
+    introduce() {
+        console.log(`Hi, my name is ${this.name} and this is my type ${this.type} `)
+    }
+}
+
+class Wizard extends Player {
+    constructor(name, type){
+        super(name,type)
+    }
+    play(){
+        console.log(`Playing:  ${this.name} of type: ${this.type} `)
+    }
+}
+
+const wizard1 = new Wizard("Shelly", "Healer")
+const wizard2 = new Wizard("Shawn", "Magic")
+//wizard1.introduce()
+//wizard2.play()
+
+//8.0 JAVASCRIPT looping
+//for
+
+for (let i = 0; i < arrVar.length; i++) {
+    sum += arrVar[i];
+    
+}
+
+
 
 
 
