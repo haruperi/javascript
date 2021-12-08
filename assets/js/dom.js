24b6db821d400fee37e8a50b922c286f8f3e47d0
haruperi <rufaroh59@gmail.com>
2021-12-08 13:32:01 +0200

    Most important DOM

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,25 +1,98 @@
 
-// DOM Selectors
-// --------------
-// getElementsByTagName
-// getElementsByClassName
-// getElementById
+/* 1 DOM Selectors
+--------------------
+getElementsByTagName
+getElementsByClassName
+getElementById
 
-// querySelector
-// querySelectorAll
+querySelector
+querySelectorAll
 
-// getAttribute
-// setAttribute
+more prefered querySelector()
+*/
 
-// ##Changing Styles
-// style.{property} //ok
+// ##It is important to CACHE selectors in variables
 
-// className //best
-// classList //best
+const body = document.body
+const text = 'This is some text'
+const grandparent = document.querySelector('#grandparent')
 
-// classList.add
-// classList.remove
-// classList.toggle
+
+/* 2. Adding Elements
+   append()
+   appendChild()             // added to a node eg divs, spans, a
+*/
+
+body.append('Page Heading')
+
+
+/* 3. Creating Elements
+	createElement()
+*/
+
+const div = document.createElement('div')
+div.textContent = `${text} inside the div`
+body.append(div)
+
+
+/* 4. Modifying Element Text
+	.textContent = 'some text'
+*/
+
+const child1 = document.querySelector('#child1')
+child1.textContent = 'This is a child'
+
+/* 5. Modifying Element HTML
+*/
+
+const h1 = document.createElement('h1')
+h1.append(text)
+div.append(h1)
+
+
+/* 6. Removing Elements
+.remove()
+*/
+const span4 = document.querySelector('#span4')
+span4.remove()
+
+
+/* 7. Modifying Element Attributes
+	  getAttribute 				//not so much of use
+      setAttribute('attri', 'value')
+	  removeAttribute('attri')
+*/
+const span2 = document.querySelector('#span2')
+span2.setAttribute('title', 'This is a title')
+span2.removeAttribute('title')
+
+
+
+/* Modifying Data Attributes
+	
+*/
+span2.dataset.newName='hie'
+
+
+
+/* 8. Modifying Element Classes
+
+	 className                       //best
+	 classList                       //best
+
+	classList.add
+	classList.remove
+	classList.toggle
+*/
+
+const child3 = document.querySelector('#child3')
+child3.classList.add('childcolor')
+
+
+/* Modifying Element Style
+	for seperation of functinality , add the css in the css file then add the class using class list
+*/
+
 
 // ##Bonus
 // innerHTML //DANGEROUS
@@ -27,41 +100,4 @@
 // parentElement
 // children
 
-// ##It is important to CACHE selectors in variables
 
-const h1 = document.querySelector("h1")
-const button = document.querySelector("#enter");
-const input = document.querySelector("#userinput");
-const ul = document.querySelector("ul");
-const cancel = document.querySelector("li")
-
-h1.className = "cooltitle"
-//cancel.className = "done"
-
-
-function inputLength() {
-	return input.value.length;
-}
-
-function createListElement() {
-	const li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-}
-
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
-}
-
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
-}
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
